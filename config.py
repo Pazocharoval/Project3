107a28d6788f408762e9264ee9587d40a7a1ab8a
Cristian Andreasyan <143656609+Pazocharoval@users.noreply.github.com>
2025-01-20 10:50:01 +0300

    Update config.py
    # Функция для сохранения результатов в файл
    def save_error_counts_to_file(error_counts, output_file):
        with open(output_file, 'w') as file:
            for timestamp, count in sorted(error_counts.items()):
                file.write(f"{timestamp} - Ошибки 404: {count}\n")
    
    # Главная функция для обработки лога и сохранения результатов
    def main():
        log_file = f'{os.getcwd()}/logs/nginx_log'  # Замените путь на путь вашего файла
        output_file = f'{os.getcwd()}/404_errors_per_minute.txt'
    
        error_counts = parse_nginx_log(log_file)
        save_error_counts_to_file(error_counts, output_file)
        print(f"Результаты сохранены в {output_file}")
    
    # Выполнение скрипта
    if __name__ == "__main__":
        main()

--- config.py
+++ config.py
@@ -1,2 +1,18 @@
-SOURCE_DIR = 'C:\\Users\\Lenovo\\Downloads\\XNXX\\AD'  # Укажите свой путь к папке с исходными файлами
-DESTINATION_DIR = 'C:\\Users\\Lenovo\\Downloads\\Xvideos'  # Укажите свой путь к папке назначения
+# Функция для сохранения результатов в файл
+def save_error_counts_to_file(error_counts, output_file):
+    with open(output_file, 'w') as file:
+        for timestamp, count in sorted(error_counts.items()):
+            file.write(f"{timestamp} - Ошибки 404: {count}\n")
+
+# Главная функция для обработки лога и сохранения результатов
+def main():
+    log_file = f'{os.getcwd()}/logs/nginx_log'  # Замените путь на путь вашего файла
+    output_file = f'{os.getcwd()}/404_errors_per_minute.txt'
+
+    error_counts = parse_nginx_log(log_file)
+    save_error_counts_to_file(error_counts, output_file)
+    print(f"Результаты сохранены в {output_file}")
+
+# Выполнение скрипта
+if __name__ == "__main__":
+    main()
